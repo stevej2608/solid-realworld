174723dfd9412c2e59f26fa9a4c1a49cfcf4d50b
Steve Jones <jonesst2608@gmail.com>
2023-06-18 16:23:50 +0100

    Tidy up

--- src/utils/tinytim.ts
+++ src/utils/tinytim.ts
@@ -1,28 +1,37 @@
 
 // https://github.com/premasagar/tim/
 
-export const tim = (template: string, data: any): string => {
+type  ILookupDict = {[token: string]: string}
+
+/**
+ * Simple template engine, see
+ *
+ * https://github.com/premasagar/tim/#tim
+ */
+
+export const tim = (template: string, data: ILookupDict): string => {
   const start = '{{'
   const end = '}}'
   const path = '[a-z0-9_][\\.a-z0-9_]*' // e.g. config.person.name
 
   const pattern = new RegExp(start + '\\s*(' + path + ')\\s*' + end, 'gi')
 
-  return template.replace(pattern, (tag, token) => {
-    const path = token.split('.') as string[]
-    const len = path.length
+  return template.replace(pattern, (tag: string, token: string) => {
+    const path = token.split('.')
 
-    let lookup = data, i = 0
+    let lookup: ILookupDict | string = data
 
-    for (; i < len; i++) {
-      lookup = lookup[path[i]]
+    for (const tok of path) {
+
+      lookup = (lookup as ILookupDict)[tok]
+
       if (lookup === undefined) {
-        throw new Error("tim: '" + path[i] + "' not found in " + tag)
+        throw new Error(`tim: '${lookup}' not found in ${tag}`)
       }
-      if (i === len - 1) {
-        return lookup
-      }
+
     }
+
+    return lookup
   })
 }
 
