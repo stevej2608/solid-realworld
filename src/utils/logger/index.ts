e6f534c11be29c0bbe028116601f883ba7499d9a
Steve Jones <jonesst2608@gmail.com>
2023-06-19 19:09:04 +0100

    Now using local browser-logger package

--- src/utils/logger/index.ts
+++ src/utils/logger/index.ts
@@ -1,6 +1,7 @@
-import { BrowserLog } from 'browser-logger'
+import { LogLevel, BrowserLog } from 'browser-logger'
 
 export const logger = new BrowserLog({
   format: '{{index}} {{timestamp}} {{title}} {{message}}{{rhs}}{{file}}:{{line}}',
-  dateformat: 'HH:MM:ss'
+  dateformat: 'HH:MM:ss',
+  level: LogLevel.INFO
 })
