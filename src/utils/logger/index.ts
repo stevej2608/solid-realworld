e2b2023b66c6a3a1c7391a1f635146879167bafc
Steve Jones <jonesst2608@gmail.com>
2023-06-23 06:55:27 +0100

    Added NULL logger fotr use when testing with jsdom

--- src/utils/logger/index.ts
+++ src/utils/logger/index.ts
@@ -1,7 +1,25 @@
 import { LogLevel, BrowserLog } from '@holonix/browser-logger'
 
-export const logger = new BrowserLog({
-  format: '{{index}} {{timestamp}} {{title}} {{message}}{{rhs}}{{file}}:{{line}}',
-  dateformat: 'HH:MM:ss',
-  level: LogLevel.INFO
-})
+const browserLogger = () => {
+  return new BrowserLog({
+    format: '{{index}} {{timestamp}} {{title}} {{message}}{{rhs}}{{file}}:{{line}}',
+    dateformat: 'HH:MM:ss',
+    level: LogLevel.INFO
+  })
+}
+
+const nullLogger = () => {
+  return {
+    info: () => {},
+    warn: () => {},
+    setLevel: () => {},
+  }
+}
+
+const isBrowser = () => {
+  return window.name !== "JSDOM"
+}
+
+const logger = isBrowser() ? browserLogger() : nullLogger()
+
+export { logger, LogLevel }
